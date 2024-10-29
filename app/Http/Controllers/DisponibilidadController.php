983e24b3aa739e0c9c61affddd2a36dedd3c6e8d
GonzaloTI <ignaciogonzaloid@gmail.com>
2024-10-29 18:29:31 -0300

    notificaciones

--- app/Http/Controllers/DisponibilidadController.php
+++ app/Http/Controllers/DisponibilidadController.php
@@ -18,7 +18,7 @@ class DisponibilidadController extends Controller
         //
         $disponibilidades = Disponibilidad::all();
         $medicos = Medico::all();
-        return view('disponibilidad.disponibilidadlist', compact('disponibilidades','medicos'));
+        return view('disponibilidad.Ddisponibilidadlist', compact('disponibilidades','medicos'));
 
     }
 
