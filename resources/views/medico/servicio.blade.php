077b8e56f7f972a5d2debce9efaa8de60492d71d
GonzaloTI <ignaciogonzaloid@gmail.com>
2024-12-05 15:40:16 -0300

    change api server

--- resources/views/medico/servicio.blade.php
+++ resources/views/medico/servicio.blade.php
@@ -66,7 +66,7 @@
 
             try {
                 // Hacer solicitud POST hacia la API
-                const response = await fetch('http://3.93.65.248/analizar-imagen/', {
+                const response = await fetch('http://34.201.46.121/analizar-imagen/', {
                     method: 'POST',
                     body: formData
                 });
